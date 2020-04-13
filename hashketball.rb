29ae03130b53635db8eade20bc39ae7e67e3b191
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-13 16:26:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,11 @@ end
 def num_points_scored(player_name)
   game_hash
   game_hash.each do |location, team_data|
-    
+    team_data.each do |attirbute, data|
+      
+    end
+      
+    end
   end
 end
 
