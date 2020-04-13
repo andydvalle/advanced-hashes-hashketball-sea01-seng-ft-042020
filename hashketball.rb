92e1c28dce8ab71291db659b471c4775a5624c08
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-13 16:57:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -158,7 +158,9 @@ def shoe_size (player_name)
       if data.class == Array
         data.each do |index|
           if index.class == Hash
-            
+            index.each do |stats, stats_value|
+              
+            end
           end
         end
       end
