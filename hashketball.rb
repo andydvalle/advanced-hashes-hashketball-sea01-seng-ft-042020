b1b5c6420025e63f21fdd4590013dac7e3559b98
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-13 17:49:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,7 +201,7 @@ def player_numbers (team_name)
   player_numbers = []
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-      team_data.each do 
+      team_data.each do |attribute, data
         
       end
     end
